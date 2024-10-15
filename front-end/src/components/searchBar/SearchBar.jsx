b60c00974b57208806ed768d70f9187f079d2d4f
Ömer Taşkın <omer_taskn@outlook.com>
2024-10-15 13:39:26 +0300

    bugs fixed

--- front-end/src/components/searchBar/SearchBar.jsx
+++ front-end/src/components/searchBar/SearchBar.jsx
@@ -1,17 +1,47 @@
-import React from 'react'
+import {useState} from 'react'
 import "./searchBar.css"
 
 const SearchBar = () => {
+  
+  const [isBuyActive, setIsBuyActive] = useState(true)
+  const [isFocused, setIsFocused] = useState(false)
+  
+  const setActiveTrue = () => {
+    if(!isBuyActive){
+      setIsBuyActive(true)
+    }
+  }
+  const setActiveFalse = () => {
+    if(isBuyActive){
+      setIsBuyActive(false)
+    }
+  }
+ 
   return (
     <div className="home-search">
       <div className="home-search-btns">
-        <button className='home-search-btn buy-btn'>Buy</button>
-        <button className='home-search-btn rent-btn'>Rent</button>
+        <button 
+          className={`home-search-btn buy-btn ${isBuyActive ? "active" : ""}`} 
+          onClick={setActiveTrue}>
+            Buy
+        </button>
+        <button 
+          className={`home-search-btn rent-btn ${isBuyActive ? "" : "active"}`}
+          onClick={setActiveFalse}>
+            Rent
+          </button>
       </div>
       <div className="home-search-container">
-        <input className='home-search-input' placeholder='Max-price' type="text" />
-        <input className='home-search-input mid-input' placeholder="Min-price" type="text" />
-        <input className='home-search-input' type="text" />
+        <input 
+          className={`home-search-input ${isFocused ? "right-hidden" : ""}`} placeholder='Location' type="text" />
+        <input 
+          className='home-search-input mid-input' 
+          placeholder="Min price" type="number" min={0} max={10000000} 
+          onFocus={() => setIsFocused(true)} 
+          onBlur={() => setIsFocused(false)}/>
+        <input 
+          className={`home-search-input ${isFocused ? "left-hidden" : ""}`} 
+          placeholder='Max price' type="number" min={0} max={10000000}/>
         <div className="home-search-icon-wrapper">
           <img className='home-search-icon' src="/icons/search.png" alt="" />
         </div>
